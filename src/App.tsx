d2cf5e207fe6d760405a869968373a755a3fd065
kashan-ahmad <kashanahmed@hwryk.com>
2023-11-06 19:03:42 +0500

    feat: added basic chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Transaction = {
+  day: string;
+  amount: number;
+};
+
+function TransactionComponent({
+  day,
+  amount,
+  percentage,
+}: Transaction & {
+  percentage: number;
+}) {
+  const isLargest = percentage >= 99.99;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button
+      style={{
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        "--height": `${percentage}%`,
+      }}
+      aria-label="Transacted Amount Per Day"
+      className={`transaction relative inline-flex flex-col gap-2 items-center justify-end w-full h-40 ${
+        isLargest ? "transaction-largest" : ""
+      }`}
+    >
+      <span className="p-1 bg-black/80 rounded-sm text-white text-[.65rem] absolute left-1/2 -translate-x-1/2 bottom-[calc(1.75rem+var(--height))]">
+        ${amount}
+      </span>
+      <span className="w-full text-gray-500 text-[.65rem]">{day}</span>
+    </button>
+  );
 }
 
-export default App
+function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  /**
+   * The percentage of each transaction based on the largest.
+   */
+  const percentages = useMemo(() => {
+    // Get the amounts from the transactions.
+    const amounts = transactions.map((transaction) => transaction.amount);
+
+    // Get the max amount.
+    const max = Math.max(...amounts);
+
+    // Using the max amount as the base, get the percentage of each amount.
+    // This will be used to set the height of each transaction.
+    return amounts.map((amount) => (amount / max) * 100);
+  }, [transactions]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      (async function () {
+        setTransactions(
+          await fetch("/transactions.json").then((res) => res.json())
+        );
+      })();
+    }, 3000);
+  }, []);
+
+  return transactions.length == 0 ? (
+    "Loading"
+  ) : (
+    <div
+      aria-label="Transactions List"
+      className="flex items-end w-full max-w-full gap-3"
+    >
+      {transactions.map((transaction, i) => (
+        <TransactionComponent {...transaction} percentage={percentages[i]} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["DM Sans", "sans-serif"],
+    },
+    extend: {
+      maxWidth: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        primary: "hsl(10, 79%, 65%)",
+        secondary: "hsl(186, 34%, 60%)",
+        neutral: {
+          DEFAULT: "hsl(33, 100%, 98%) ",
+          100: "hsl(27, 66%, 92%)",
+          200: "hsl(28, 10%, 53%)",
+          300: "hsl(25, 47%, 15%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
